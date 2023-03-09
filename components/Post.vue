90fdf25434b278e4408b51d58ed03341d2d13490
ssungs <sws7420@gmail.com>
2023-03-09 22:27:53 +0900

    project setup & create Index page

---
+++ components/Post.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="z-50 bottom-0 h-full w-full">
+        <div class="py-2 w-full">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center text-white">
+                    <img :src="post.image" alt="" class="rounded-full h-[35px]">
+                    <div class="ml-2 font-semibold text-[18px]">{{ post.name }}</div>
+                </div>
+
+                <div @click="isMenu = !isMenu" class="relative">
+                    <button 
+                        :disabled="isDeleting"
+                        class="flex items-center text-white p-1 h-[24px] w-[24px] hover:bg-gray-800 rounded-full cursor-pointer" 
+                        :class="isMenu ? 'bg-gray-800' : ''"
+                    >
+                        <Icon v-if="!isDeleting" name="bi:three-dots" color="#ffffff" size="18"/>
+                        <Icon v-else name="eos-icons:bubble-loading" color="#ffffff" size="18"/>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useUserStore } from '~/store/user';
+const userStore = useUserStore();
+
+const runtimeConfig = useRuntimeConfig();
+let isMenu = ref(false)
+let isLike = ref(false)
+let isDeleting = ref(false)
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <MainLayout>
+        <div class="w-full overflow-auto" id="IndexPage">
+            <div class="mx-auto max-w-[500px] overflow-hidden">
+                <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+                    <div class="text-white" v-if="isPosts" v-for="post in posts" :key="post">
+                        {{ post }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import { onBeforeMount } from 'vue';
+import MainLayout from '~/layouts/MainLayout.vue';
+
+import { useUserStore } from '../stores/user';
+const userStore = useUserStore();
+
+let posts = ref([])
+let isPosts = ref(true)
+let isLoading = ref(false)
+
+onBeforeMount(() => {
+    posts.value = [
+        {
+            name: 'Shin Woosung',
+            image: 'https:/placehold.co/100',
+            text: 'This is the title',
+            picture: 'https:/placehold.co/500',
+        }
+    ]
+});
+</script>
+
+<style lang="sass" scoped>
+
+</style>
