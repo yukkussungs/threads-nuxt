63f234348de6b1b22c1c3574930ffed1ad9f96f9
ssungs <sws7420@gmail.com>
2023-03-15 11:09:45 +0900

    feature add post components

--- components/Post.vue
+++ components/Post.vue
@@ -16,6 +16,19 @@
                         <Icon v-if="!isDeleting" name="bi:three-dots" color="#ffffff" size="18"/>
                         <Icon v-else name="eos-icons:bubble-loading" color="#ffffff" size="18"/>
                     </button>
+                    <div v-if="isMenu" class="absolute border border-gray-600 right-0 z-20 mt-1 rounded">
+                        <button
+                            class="flex items-center rounnded gap-2 text-red-500 justify-between bg-black w-full pl-4 pr-3 py-1 hover:bg-gray-900"
+                        >
+                        <div>Delete</div>
+                        <Icon name="solar:trash-bin-trash-broken" size="20" />
+                    </button>
+                    </div>
+                </div>
+            </div>
+            <div class="relative flex items-center w-full">
+                <div class="w-[42px] mx-auto">
+                    <div class="absolute ml-4 mt-1 top-0 w-[1px] bg-gray-700 h-full"></div>
                 </div>
             </div>
         </div>
